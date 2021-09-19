8b70b324446b1e8bb0e159e4d4f0f19755acfcf6
Tom Hirschberger <git@hirschberger.eu>
2021-09-19 13:18:57 +0200

    again tried to fix the error 498 during fetch of the live streams of the cams;

--- node_helper.js
+++ node_helper.js
@@ -159,10 +159,27 @@ module.exports = NodeHelper.create({
         } else if (error){
           console.log(self.name+": Problem during fetch of cams of ds with idx: "+curDsIdx)
           console.log(self.name+": "+JSON.stringify(error, null, 2))
-          if ((typeof error["code"] !== "undefined") &&
-              ((error["code"] === 105) || (error["code"] === 498)) &&
-              (self.config.skipOnPrivilegeError)){
-            console.log(self.name+": Got privilege error but skipping is activated!")
+          if (typeof error["code"] !== "undefined"){
+            if(
+              (error["code"] === 105) || 
+              (error["code"] === 498)
+            ){
+              if (self.config.skipOnPrivilegeError){
+                console.log(self.name+": Got privilege error but skipping is activated!")
+              } else {
+                console.log("Skipping is disabled. Sending empty list of streams!")
+                self.sendSocketNotification("DS_STREAM_INFO",{
+                  dsIdx: curDsIdx,
+                  camStreams: {},
+                })
+              }
+            } else {
+              console.log("Unknown error. Sending empty list of streams!")
+              self.sendSocketNotification("DS_STREAM_INFO",{
+                dsIdx: curDsIdx,
+                camStreams: {},
+              })
+            }
           } else {
             self.sendSocketNotification("DS_STREAM_INFO",{
               dsIdx: curDsIdx,
